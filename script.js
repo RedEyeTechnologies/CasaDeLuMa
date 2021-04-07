5f1abdb5af38230c592407ba30cb9bdbc83addbb
Jeff C <coen.jeffery@outlook.com>
2021-04-07 02:40:56 -0700

    media querries updated

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+// var address= document.getElementById("endteredAddress").innerHTML;
+
 function openForm() {
     document.getElementById("popupForm").style.display = "block";
+
+    // document.getElementById("address").innerHTML = "example"; 
 }
 function closeForm() {
     document.getElementById("popupForm").style.display = "none";
 }
-function addressFill() {
-  var address = document.getElementById("enteredAddress").value; 
 
-  document.getElementById("enteredAddress").value = address;
-}
