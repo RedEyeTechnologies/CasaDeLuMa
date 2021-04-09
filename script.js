76cb15b6fc8bf70b585779734af5e3a7a0ba32fc
Jeff C <coen.jeffery@outlook.com>
2021-04-08 17:37:57 -0700

    changed html to match client's information

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// var address= document.getElementById("endteredAddress").innerHTML;
 
 function openForm() {
     document.getElementById("popupForm").style.display = "block";
-
-    // document.getElementById("address").innerHTML = "example"; 
 }
 function closeForm() {
     document.getElementById("popupForm").style.display = "none";
